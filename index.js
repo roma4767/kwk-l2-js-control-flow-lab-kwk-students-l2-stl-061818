f82c0c8acb9b725c08be24f7dd5d4ad6b51fd459
Romina D-R <rdr2711@yahoo.com>
2018-06-22 01:12:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your code in this file!
+function scuberGreetingForFeet(rideFeet) {
+  if (rideFeet <= 400) {
+    return 'This one is on me!'
+  }
+  else if (rideFeet > 2500) {
+    return 'No can do.'
+  }
+  else if (rideFeet > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
+}
+
+function ternaryCheckCity(city) {
+  if (city === 'NYC') {
+    return 'Ok, sounds good.'
+  } 
+  else {
+    return 'No go.'
+  }
+}
+
+function switchOnCharmFromTip(tipAmount) {
+  if (tipAmount === 'generous') {
+    return 'Thank you so much.'
+  }
+  else if (tipAmount === 'not as generous') {
+    return 'Thank you.'
+  }
+  else {
+    return 'Bye.'
+  }
+}
